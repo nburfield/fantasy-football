ef3e96d059c022ec3154428a617f8cf14a13ad8e
nolan <nolan@bcinnovationsonline.com>
2022-08-16 19:50:33 -0700

    added in draft board build

--- make_draft_board.py
+++ make_draft_board.py
@@ -10,11 +10,44 @@ import os
 import csv
 import json
 from datetime import datetime
+from jinja2 import Environment, FileSystemLoader
 import requests
 
 
+# Used for ARGS validation
 SCORE_FORMATS = ["ppr", "half-ppr", "standard"]
 TEAMS = ["8", "10", "12", "14"]
+
+# Used to map player names for consistency among platforms
+PLAYER_NAME_MAP = {"Travis Etienne Jr.": "travis_etienne",
+                   "Travis Etienne": "travis_etienne",
+                   "Melvin Gordon III": "melvin_gordon",
+                   "Melvin Gordon": "melvin_gordon",
+                   "Ronald Jones II": "ronald_jones",
+                   "Ronald Jones": "ronald_jones",
+                   "Darrell Henderson": "darrell_henderson",
+                   "Darrell Henderson Jr.": "darrell_henderson",
+                   "Mark Ingram": "mark_ingram",
+                   "Mark Ingram II": "mark_ingram",
+                   "Isaih Pacheco": "isiah_pacheco",
+                   "Isiah Pacheco": "isiah_pacheco",
+                   "Brian Robinson Jr.": "brian_robinson",
+                   "Brian Robinson": "brian_robinson",
+                   "Jeffery Wilson": "jeffery_wilson",
+                   "Jeff Wilson Jr.": "jeffery_wilson",
+                   "Allen Robinson": "allen_robinson",
+                   "Allen Robinson II": "allen_robinson",
+                   "Gabriel Davis": "gabe_davis",
+                   "Gabe Davis": "gabe_davis",
+                   "Josh Palmer": "josh_palmer",
+                   "Joshua Palmer": "josh_palmer",
+                   "Robby Anderson": "robbie_anderson",
+                   "Robbie Anderson": "robbie_anderson",
+                   "Marvin Jones": "marvin_jones",
+                   "Marvin Jones Jr.": "marvin_jones",
+                   "Robert Tonyan Jr.": "robert_tonyan",
+                   "Robert Tonyan": "robert_tonyan"
+                  }
 
 
 def get_adp_data(datamap):
@@ -45,7 +78,18 @@ def get_adp_data(datamap):
         adp_r_json = adp_r.json()
         if adp_r_json.get("status", "bad") == "Success":
             for player in adp_r_json["players"]:
-                key = player["name"].replace(' ', '_').lower()
+                # Make the Dict key from player name
+                key = player["name"].replace(' ', '_').replace('.', '').lower()
+
+                # Check for a key mapping with this player
+                if player["name"] in PLAYER_NAME_MAP:
+                    key = PLAYER_NAME_MAP[player["name"]]
+
+                # Log when duplicate players are found
+                if key in adp_data:
+                    logging.error("%s has duplicate hits.", player["name"])
+
+                # Set the player key/value
                 adp_data[key] = player
         else:
             logging.error("Bad API call with Status: %s", adp_r_json.get("status"))
@@ -81,7 +125,11 @@ def add_player_rankings(datamap, adp_data):
             player_list = csv.DictReader(pr_f)
             for player in player_list:
                 # Get the Data key and verify it exists
-                key = player["Name"].replace(' ', '_').lower()
+                key = player["Name"].replace(' ', '_').replace('.', '').lower()
+
+                # Check for a key mapping with this player
+                if player["Name"] in PLAYER_NAME_MAP:
+                    key = PLAYER_NAME_MAP[player["Name"]]
 
                 # Log and skip on error
                 if not key in adp_data:
@@ -98,6 +146,54 @@ def add_player_rankings(datamap, adp_data):
                     logging.error("Bad Values for %s : %s", player["Name"], str(ex))
 
 
+def organize_db_data(adp_data):
+    """
+    organize_db_data Sorts and Categorize ADP data for draft board.
+
+    :param adp_data: The dict with ADP and rankings
+    :return A dict of key positions with sorted lists
+    """
+
+    # Init the return data
+    db_data = {"qb": [], "rb": [], "wr": [], "te": []}
+
+    # Build the position lists
+    for player_k in adp_data:
+        position_k = adp_data[player_k]["position"].lower()
+
+        # Check the position is valid. Skips Kickers and Defense
+        if position_k in {'def', 'pk'}:
+            continue
+
+        # Postion in Draft Board Dict
+        if position_k in db_data:
+            db_data[position_k].append(adp_data[player_k])
+        else:
+            logging.error("Position %s not found", position_k)
+
+    # Sork the position lists
+    for position_k in db_data:
+        db_data[position_k] = sorted(db_data[position_k], key=lambda d: d["adp"])
+
+    return db_data
+
+
+def generate_html_v1(datamap, draft_board_data):
+    """
+    generate_html_v1 Version 1 of the HTML draftboard file
+    """
+
+    # Setup the jinja2 Environment
+    environment = Environment(loader=FileSystemLoader("./templates/"))
+    template = environment.get_template("draftboard.html")
+
+    content = template.render(datamap=datamap, draft_board_data=draft_board_data)
+    file_name = "./files/"
+    file_name += f"db_{datamap['year']}_{datamap['scoring_format']}_{datamap['player_count']}.html"
+    with open(file_name, mode="w", encoding="utf-8") as message:
+        message.write(content)
+
+
 def main():
     """
     main The Main function of the script
@@ -112,6 +208,7 @@ def main():
                         choices=SCORE_FORMATS, required=True, metavar="<scoring_format>")
     parser.add_argument('-pc', '--player_count', help="the fantasy players of ADP data",
                         choices=TEAMS, required=True, metavar="<player_count>")
+    parser.add_argument('-cc', '--clear_cache', help="clears the cached data", action='store_true')
 
     args = parser.parse_args()
 
@@ -123,15 +220,31 @@ def main():
         "year": datetime.now().year
     }
 
-    # Get the ADP data to dict
-    adp_data = get_adp_data(datamap)
+    run_info = f"{datamap['scoring_format']}_{datamap['player_count']}_{datamap['year']}"
+    export_json = f"./files/draft_board_data_{run_info}.json"
+    adp_data = {}
 
-    # Merge in Player Rankings
-    add_player_rankings(datamap, adp_data)
+    # Check for the Cache file to exist. Load if so.
+    if os.path.isfile(export_json) and args.clear_cache is False:
+        logging.error("======= LOADING DATA FROM CACHE (clear with flag -cc) =======")
+        with open(export_json, "r", encoding="utf-8") as db_f:
+            adp_data = json.load(db_f)
+    else:
+        # Get the ADP data to dict
+        adp_data = get_adp_data(datamap)
 
-    # Save out the data to file
-    with open("files/draft_board_data.json", "w", encoding="utf-8") as db_f:
-        db_f.write(json.dumps(adp_data))
+        # Merge in Player Rankings
+        add_player_rankings(datamap, adp_data)
+
+        # Save out the data to file
+        with open(export_json, "w", encoding="utf-8") as db_f:
+            db_f.write(json.dumps(adp_data, indent=4))
+
+    # Build the draft board data
+    draft_board_data = organize_db_data(adp_data)
+
+    # Generate the HTML output
+    generate_html_v1(datamap, draft_board_data)
 
 
 if __name__ == "__main__":
